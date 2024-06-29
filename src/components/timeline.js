46dfb765e1463856f471c5312c85732b4639021e
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-06-29 19:24:52 +0530

    alpha3

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -7,7 +7,7 @@ import * as icon from '@coreui/icons';
 
 export default function Timeline() {
     return (
-        <div className="lg:p-16 p-8 ">
+        <div className="lg:p-16 p-4 ">
             <div className="flex flex-col lg:flex-row  justify-between items-center">
                 <h1 className="font-bold text-6xl mb-8">My Projects</h1>
                 <Link href={'/'} className="flex gap-2 items-center justify-center text-center bg-yellow-200 hover:translate-x-1 hover:translate-y-1 hover:shadow-none transition-all font-bold text-xl rounded-full px-6 py-4 border-2 border-black shadow-[4px_4px_0px_rgba(0,0,0,1)]">
